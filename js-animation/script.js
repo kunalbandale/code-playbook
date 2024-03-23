78bf1a193c5811351f3dc912697acf4862314a2d
kunalbandale <bandalekunal@gmail.com>
2024-03-23 22:20:34 +0530

    Animation in js

---
+++ js-animation/script.js
@@ -0,0 +1,46 @@
+// gsap.to("#box", {
+//     x:1000,
+//     rotate:360,
+//     backgroundColor:"blue",
+//     duration:2,
+//     dealy:1
+// })
+
+// gsap.from("#box", {
+//     x:1000,
+//     rotate:360,
+//     backgroundColor:"blue",
+//     duration:2,
+//     dealy:1
+
+// })
+
+// GSAP Timeline
+
+var tl = gsap.timeline()
+
+tl.to("#box1", {
+    x:1200,
+    rotate:360,
+    scale:0.5,
+    duration:3,
+    // delay:1
+})
+
+
+tl.to("#box2", {
+    x:1200,
+    rotate:360,
+    scale:0.5,
+    duration:3,
+    // delay:2
+})
+
+
+tl.to("#box3", {
+    x:1200,
+    rotate:360,
+    scale:0.5,
+    duration:3,
+    // delay:3
+})
