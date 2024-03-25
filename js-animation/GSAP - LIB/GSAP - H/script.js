393eebb1b38a83057e61117515e32c86c6f679d0
kunalbandale <bandalekunal@gmail.com>
2024-03-25 16:58:39 +0530

    files updated

--- js-animation/GSAP - LIB/GSAP - H/script.js
+++ js-animation/GSAP - LIB/GSAP - H/script.js
@@ -1,13 +1,12 @@
-gsap.to("#page2 img", {
-    width:"100%",
-    scrollTrigger: {
-        trigger:"#page2",
+gsap.to("#page2 h1", {
+    transform: "translate(-125%)",
+    scrollTrigger:{
+        trigger:"page2",
         scroller:"body",
         markers:true,
         start:"top 0",
         end:"top -100%",
-        scrub:2,
+        scrub:5,
         pin:true
     }
-
 })
---
+++ library locomotive/script.js
@@ -0,0 +1,8 @@
+
+// import LocomotiveScroll from 'locomotive-scroll';
+const scroll = new LocomotiveScroll();
+const target = document.querySelector('#main');
+
+scroll.scrollTo(target);
+
+// scroll speed
